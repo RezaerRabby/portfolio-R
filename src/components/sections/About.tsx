975467a372a308fda72a851b784f26543cf5b9a3
Rezae Rabby <rezaerrabby@gmail.com>
2026-06-24 21:39:49 +0600

    cv pdf done

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -47,7 +47,7 @@ export default function About() {
               </a>
 
               <a
-                href="/Rezaer-Rabby-CV.pdf"
+                href="/Rezaer-Rabby.pdf"
                 download
                 className="border border-white/20 px-6 py-3 rounded-xl hover:border-cyan-500 transition"
               >
