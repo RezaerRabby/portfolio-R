38443297930f5f0d5c83ebea06a64ac3f230c4bb
Rezae Rabby <rezaerrabby@gmail.com>
2026-06-24 18:32:21 +0600

    contact done

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -1,18 +1,4 @@
-// export default function Contact() {
-//   return (
-//     <section id="contact" className="py-24 bg-slate-950 text-white">
-//       <div className="max-w-6xl mx-auto px-5">
-//         <h2 className="text-4xl font-bold mb-6">
-//           Contact
-//         </h2>
 
-//         <p>Email: rezaerrabby@gmail.com</p>
-//         <p>Phone: +880 1760 137770</p>
-//         <p>Address: Dhaka, Bangladesh</p>
-//       </div>
-//     </section>
-//   );
-// }
 
 
 export default function Contact() {
